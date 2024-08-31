2f3e5f538c531522deed4de6b0fede9d8b27e612
Kishan Dalwadi <dalwadikishan456@gmail.com>
2024-08-31 23:50:50 +0530

    Check that method add exists on StringCalculator class

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,4 +2,9 @@ require 'rspec'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+  describe "#add" do
+    it "checks method #add exists or not" do
+      expect( StringCalculator.instance_methods.include?(:add) ).to eq(true)
+    end
+  end
 end
