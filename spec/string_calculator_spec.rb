64d99c5ca7d22a2dbb47c38e22e203acb7f0d7ec
Kishan Dalwadi <dalwadikishan456@gmail.com>
2024-09-01 00:14:54 +0530

    Handle single number input and extracted calculator instance in spec

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,18 +3,26 @@ require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe "#add" do
+    let(:calculator) { StringCalculator.new }
+
     it "checks method exists or not" do
-      expect( StringCalculator.instance_methods.include?(:add) ).to eq(true)
+      expect( calculator.methods.include?(:add) ).to eq(true)
     end
 
     it "checks method accepts one argument" do
-      calculator = StringCalculator.new
       expect { calculator.add }.to raise_error(ArgumentError)
     end
 
     it "handles that empty string input returns 0" do
-      calculator = StringCalculator.new
       expect(calculator.add('')).to eq(0)
+    end
+
+    it "returns the number itself when only single number is given" do
+      expect(calculator.add('100')).to eq(100)
+    end
+
+    it "checks output is Number when valid single number is passed" do
+      expect(calculator.add('55').class).to eq(Integer)
     end
   end
 end
