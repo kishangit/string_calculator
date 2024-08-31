7f9fc85221ace71efa5a3597f442ef65fad0ad5f
Kishan Dalwadi <dalwadikishan456@gmail.com>
2024-09-01 00:02:33 +0530

    Handle empty string input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,8 +3,18 @@ require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe "#add" do
-    it "checks method #add exists or not" do
+    it "checks method exists or not" do
       expect( StringCalculator.instance_methods.include?(:add) ).to eq(true)
+    end
+
+    it "checks method accepts one argument" do
+      calculator = StringCalculator.new
+      expect { calculator.add }.to raise_error(ArgumentError)
+    end
+
+    it "handles that empty string input returns 0" do
+      calculator = StringCalculator.new
+      expect(calculator.add('')).to eq(0)
     end
   end
 end
